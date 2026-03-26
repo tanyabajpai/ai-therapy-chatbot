3b48b199acbf44960ef1a848cd702bb533716a09
Tanya Bajpai <summervacation1403@gmail.com>
2026-03-27 04:37:24 +0530

    fix python version and remove problematic imports

--- voice.py
+++ voice.py
@@ -1,11 +1,2 @@
-import speech_recognition as sr
-
 def get_voice_input():
-    try:
-        import speech_recognition as sr
-        r = sr.Recognizer()
-        with sr.Microphone() as source:
-            audio = r.listen(source)
-        return r.recognize_google(audio)
-    except Exception:
-        return None
+    return None
